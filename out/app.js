3b97b9c35a577e14b21c10b22f9d64377d180bcd
Liv Mari <liv.alterskjaer@icloud.com>
2024-09-11 13:45:52 +0200

    initial commit

---
+++ out/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
+const app = express();
+app.use(cors());
+app.use(helmet());
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    },
+});
+io.on('connection', socket => {
+    socket.on('message', message => {
+        console.log(`A user said: ${message}`);
+        io.emit('message', message);
+    });
+});
+server.listen(8080, () => {
+    console.log('server is running on port 8080');
+});
